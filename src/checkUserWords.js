44f36519b2849976ef0cb187f4e873c7c29766f7
Simmarith <menzeladrian@googlemail.com>
2020-04-30 03:18:05 +0200

    muteUserWord now works on embeds

--- src/checkUserWords.js
+++ src/checkUserWords.js
@@ -15,8 +15,29 @@ module.exports = (client) => {
 
     if (words.find(word => (message.content.indexOf(word.word) !== -1)) !== undefined) {
       message.delete()
+      return
     }
-  }
+
+    // check embeds
+    function hasWordInEmbed(embed, word) {
+      const relevantStrings = [
+        embed.title,
+        embed.footer.text
+      ]
+      embed.fields.forEach(field => {
+        relevantStrings.push(field.name)
+        relevantStrings.push(field.value)
+      })
+      return (relevantStrings.find(line => (line.indexOf(word.word) !== -1)) !== undefined)
+    }
+    if (message.embeds != null) {
+      if (message.embeds.find(embed => {
+        return words.find(word => hasWordInEmbed(embed, word)) !== undefined
+      }) !== undefined) {
+        message.delete()
+      }
+    }
+  } 
 
   client.on('message', checkMessage)
 } 
