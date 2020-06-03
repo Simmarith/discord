f4047d741876f663cdc93dd909e8daacafb8c809
Simmarith <menzeladrian@googlemail.com>
2020-06-03 17:01:50 +0200

    Use better search site for lego command

--- src/commands/lego.js
+++ src/commands/lego.js
@@ -4,6 +4,6 @@ module.exports = function (message, onlyPayload) {
   if (!/^\d{4,5}$/.test(onlyPayload)) {
     getPrefix(message).then(prefix => message.channel.send(`\`${prefix}lego <inventory number>\``))
   } else {
-    message.channel.send(`https://www.lego.com/en-us/product/${onlyPayload}`)
+    message.channel.send(`https://brickset.com/sets/${onlyPayload}`)
   }
 } 
